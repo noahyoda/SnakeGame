b0dae1265592119f1fcf76785d652b0e78315797
noahyoda <noahyoda1@gmail.com>
2021-08-11 11:55:31 -1000

    comments and removed useless method

--- Game.java
+++ Game.java
@@ -18,85 +18,80 @@ import javax.swing.JPanel;
  * 
  * @author Noahyoda
  * 
- * TODO:
- * add buttons back to frame later
- * setup grid coordinate system
- * make snake class
- * add food to frame
- * make food appear at random x/y
+ *         TODO: add buttons back to frame later setup grid coordinate system
+ *         make snake class add food to frame make food appear at random x/y
  *
  */
 
 public class Game extends JFrame implements ActionListener {
-	
+
 	private JButton pause, speedUp, slowDown;
 	private JFrame frame;
 	private static final int width = 400, height = 400;
-	//cells are in steps of 40
+	// cells are in steps of 40
 	private int fx, fy;
 
 	public static void main(String[] args) {
 		new Game();
 	}
-	
+
+	/**
+	 * this is the main driving method for the game
+	 */
 	public Game() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame = new JFrame();
 		Container content = frame.getContentPane();
 		content.setLayout(new BorderLayout());
 		frame.setPreferredSize(new Dimension(width, height));
-		
+
 		SplittableRandom rng = new SplittableRandom();
-//		fx = rng.nextInt(10);
-//		fy = rng.nextInt(10);
+		// fx = rng.nextInt(10);
+		// fy = rng.nextInt(10);
 		fx = 4;
 		fy = 4;
 		Food food = new Food(fx * 10, fy * 10);
-		
-		JPanel buttons = buttons();
-		JPanel gamePanel = gamePanel();
-		
+
+		// button elements are commented out for the time being to focus on the games
+		// main aspects first
+		// JPanel buttons = buttons();
+		JPanel gamePanel = new JPanel();
+
 		content.add(gamePanel);
-		//content.add(buttons);
+		// content.add(buttons);
 		content.getComponent(0).setBackground(new Color(10).black);
-		
-		
+
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	}
-	
-	public JPanel gamePanel() {
-		JPanel pain = new JPanel();
-		return pain;
-	}
-	
+
 	public JPanel buttons() {
 		JPanel buttonBar = new JPanel();
 		Color btnColor = new Color(10).cyan;
-		
+
 		pause = new JButton();
 		pause.setBackground(btnColor);
 		speedUp = new JButton();
 		speedUp.setBackground(btnColor);
 		slowDown = new JButton();
 		slowDown.setBackground(btnColor);
-		
+
 		buttonBar.add(pause);
 		buttonBar.add(speedUp);
 		buttonBar.add(slowDown);
-		
+
 		return buttonBar;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
