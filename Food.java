ee49e8364a5bd54b033b1f89e539b47a6c0d28b7
noahyoda <noahyoda1@gmail.com>
2021-08-11 11:57:43 -1000

    comments

--- Food.java
+++ Food.java
@@ -9,18 +9,29 @@ public class Food {
 
 	public int x, y;
 	private final int width = 20, height = 20;
-	
+
+	/**
+	 * constructor for food object that is located at the given x and y coordinates
+	 * 
+	 * @param x location of food
+	 * @param y location of food
+	 */
 	public Food(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
+	/**
+	 * graphic class that designs the food to be a red sphere
+	 * 
+	 * @param graphics object to create the sphere from
+	 */
 	public void draw(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		Ellipse2D.Double circle = new Ellipse2D.Double(x, y, width, height);
-		
+
 		g2d.setColor(Color.red);
 		g2d.fill(circle);
 	}
-	
+
 }
